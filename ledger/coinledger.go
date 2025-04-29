6791966853dfd0e4c1b732f230525ab0097abbca
Fuzzycc-local1 <70171293+Fuzzycc@users.noreply.github.com>
2025-04-30 01:09:43 +0300

    Changes to be committed: 	new file:   data/coin.sample.scsv 	modified:   data/coin.sample.txt 	modified:   ledger/coinledger.go 	modified:   main.go
    A lot of progress was made today in terms of envisioning
    The fog is cleared
    Get it ready, get it fast. refine later.

--- ledger/coinledger.go
+++ ledger/coinledger.go
@@ -1,76 +1,122 @@
 package ledger
 
 import (
-	"bufio"
-	"fmt"
+	"encoding/csv"
 	"io"
-	"os"
+	"sync"
 )
 
-func Open(name string) (*bufio.Reader, func() error, error) {
-	f, err := os.Open(name)
-	if err != nil {
-		return nil, nil, err
+/*
+Coin Rules
+Name must be 50 characters = 50 bytes,so
+Version;ID;Name;Value
+Version 1 byte
+ID 4 bytes
+Name 50 bytes
+Value 10 bytes (uint32 is 4 billion)
+semicolons 3 bytes
+line is 1+4+50+10+3
+Total is 68 bytes
+
+What I need:
+TextAppender, LineReader
+*/
+
+
+type BaseReader struct {
+	Comma   rune
+	Comment rune
+	cR      *csv.Reader
+}
+
+func NewBaseReader(r io.Reader) *BaseReader {
+	rr := csv.NewReader(r)
+	br := &BaseReader{
+		Comma:   ';',
+		Comment: '0',
+		cR:      rr,
 	}
-	r := bufio.NewReader(f)
-	closeFn := func() error {
-		err := f.Close()
+	br.cR.Comma = br.Comma
+	br.cR.Comment = br.Comment
+	return br
+}
+
+// func (r *Reader) FieldPos(field int) (line, column int)
+// func (r *Reader) InputOffset() int64
+
+func (r *BaseReader) Read() (record []string, err error) {
+	return r.cR.Read()
+}
+
+func (r *BaseReader) ReadAll() (records [][]string, err error) {
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			return records, nil
+		}
 		if err != nil {
-			return err
+			return nil, err
 		}
-		return nil
+		records = append(records, record)
 	}
-	return r, closeFn, nil
+	// taken from r.cR.ReadAll()
+	// why do this though?
 }
 
-func Read(r *bufio.Reader) (index uint, line string) {
-	var i uint = 0
-	for {
-		l, erx := r.ReadString('\n')
-
-		if l != "" {
-			// version-based parse and return index and line
-			// if version is 0, continue
-			i++
-			return i, l
-		}
-
-		switch erx {
-		case io.EOF:
-			return 0, "EOF"
-		case nil:
-			continue
-		default:
-			return 0, ""
-		}
-	}
+type BaseWriter struct {
+	// comma rune
+	cW *csv.Writer
+	m  sync.Mutex
 }
 
-func parseCoin(line string)
+func (bw *BaseWriter) SetComma(comma rune) {
+	// bw.comma = comma
+	bw.cW.Comma = comma
+}
 
-func ReadAll(file string) {
-	f, err := os.Open(file)
-	if err != nil {
-		panic(err)
+func NewBaseWriter(w io.Writer) *BaseWriter {
+	ww := csv.NewWriter(w)
+	bw := &BaseWriter{
+		// comma: ';',
+		cW: ww,
 	}
-	defer f.Close()
+	bw.cW.Comma = ';'
+	return bw
+}
 
-	r := bufio.NewReader(f)
-	for {
-		// read from buffer
-		line, err := r.ReadString('\n')
+func (bw *BaseWriter) SetCRLF(useCRLF bool) {
+	bw.cW.UseCRLF = useCRLF
+}
 
-		if line != "" {
-			fmt.Println(line)
-		}
+// func (bw *BaseWriter) Error() error
 
-		switch err {
-		case io.EOF:
-			break
-		case nil:
-			continue
-		default:
-			panic(err)
-		}
-	}
+func (bw *BaseWriter) Flush() {
+	bw.m.Lock()
+	defer bw.m.Unlock()
+	bw.cW.Flush()
+}
+
+func (bw *BaseWriter) Write(record []string) error {
+	bw.m.Lock()
+	defer bw.m.Unlock()
+	record[2] = "\"" + record[2] + "\""
+	return bw.cW.Write(record)
+}
+
+func (bw *BaseWriter) WriteAll(records [][]string) error {
+	bw.m.Lock()
+	defer bw.m.Unlock()
+	// for i := range len(records) {
+	// 	records[i][2] = `'` + records[i][2] + `'`
+	// 	// for some reason, using " results in 3 quotes on each side. I have no idea why
+	// 	// turns out quotes, Comma, and space at the start are quoted. So a quote is quoted and becomes three.
+	// }
+	return bw.cW.WriteAll(records)
+}
+
+
+func CleanBase(r io.Reader, w io.Writer) {
+	// read the cache
+	// read the file
+	// loop the file, matching it against the cache
 }
--- main.go
+++ main.go
@@ -2,81 +2,164 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"text/tabwriter"
+
+	"github.com/Fuzzycc/coinme/ledger"
 )
+
+/*
+In utility, make a Coin Marshal to BaseLedger, which is a record []string in the V1 form
+Define the V1 form and abstract the code? Nah, just write it to work and refine it later.
+or should the marsha function be in the ledger?
+The ledger would become too big later on if I have multipel forms, each with their own marshal unmarshal
+
+so, to write a coin... r.Write(utils.MarshalV1SCSV(coin)) V1 denonates the rules on the field.
+So there can be V1SCSV for the base, V1JSON, V1XML, V1SQl, etc.
+
+For base.
+New coins are appended only, sorting is separate
+So, Cleaning needs to just discard any occurence beyond the first
+Because, manually editing the file is the only way to get ID duplication
+Because the CLI does not allow ID insertion
+*/
 
 func main() {
+	// still in development, this is verification code
 	fmt.Println("Coinme, right now!")
+
+	// Open sample file
+	file, err := os.OpenInRoot("./data/", "coin.sample.txt")
+	if err != nil {
+		panic(err)
+	}
+	// defer file.Close()
+
+	// create BaseReader, an enconding/csv Reader wrapper
+	r := ledger.NewBaseReader(file)
+	// l.Comma = ';'
+	// l.Comment = '0'
+
+	// Read all entries. Read also exist
+	records, err := r.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	// close the file
+	file.Close()
+
+	// open the file for writing
+	root, _ := os.OpenRoot("./data/")
+	f, err := root.OpenFile("coin.sample.scsv", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0o644)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	// create tmp file as a data store
+	// f, err := os.CreateTemp("", "coinme-tmpfile-*.txt")
+	// defer f.Close()
+	// defer os.Remove(f.Name())
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer f.Close()
+
+	// write to the file
+	w := ledger.NewBaseWriter(f)
+	w.SetComma(';')
+	w.WriteAll(records)
+
+	f.Seek(0, 0)
+	r = ledger.NewBaseReader(f)
+
+	// Read from the file
+	records, err = r.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	// display results
+	display := func() {
+		w := tabwriter.NewWriter(os.Stdout, 1, 1, 4, ' ', 0)
+		defer w.Flush() //<<< printing is here
+		for _, record := range records {
+			fmt.Fprintf(w, "%+v\t\"%+v\"\t%+v\n", record[1], record[2], record[3])
+		}
+		fmt.Println(f.Name())
+	}
+	display()
 }
 
-type (
-	// Abstraction for the underlying coin Id type
-	CoinID uint16
-	// Abstraction for the underlying coin Value type
-	CoinValue uint32
-	// Abstraction for the underlying coin Name type
-	CoinName string
-	// Abstraction for the underlying chain Id type
-	ChainID uint16
-	// Abstraction for the underlying chain Name type
-	ChainName string
-)
+// type (
+// 	// Abstraction for the underlying coin Id type
+// 	CoinID uint16
+// 	// Abstraction for the underlying coin Value type
+// 	CoinValue uint32
+// 	// Abstraction for the underlying coin Name type
+// 	CoinName string
+// 	// Abstraction for the underlying chain Id type
+// 	ChainID uint16
+// 	// Abstraction for the underlying chain Name type
+// 	ChainName string
+// )
 
-// A coin has a Value and a Name.
-type coin struct {
-	Id    CoinID
-	Value CoinValue
-	Name  CoinName
-}
-// Underlying Data structure
-// Coin ID, Value, Name
+// // A coin has a Value and a Name.
+// type coin struct {
+// 	Id    CoinID
+// 	Value CoinValue
+// 	Name  CoinName
+// }
 
-// A chain is a named collection of coins by their Id value.
-type chain struct {
-	Id    ChainID
-	Name  ChainName
-	Coins []CoinID
-}
+// // Underlying Data structure
+// // Coin ID, Value, Name
 
-type coinTable []coin
+// // A chain is a named collection of coins by their Id value.
+// type chain struct {
+// 	Id    ChainID
+// 	Name  ChainName
+// 	Coins []CoinID
+// }
 
-type chainTable []chain
+// type coinTable []coin
 
-// Init methods---------------------------
-func NewCoin(s CoinName, v CoinValue) (coin)
-func NewChain(s ChainName,coins ...any) (chain)
+// type chainTable []chain
 
+// // Init methods---------------------------
+// func NewCoin(s CoinName, v CoinValue) coin
+// func NewChain(s ChainName, coins ...any) chain
 
-// Chain methods---------------------------
+// // Chain methods---------------------------
 
-func (c chain) Add(...any) error // for each coin, if switch type coin then add coin.id, if uint then add the id
+// func (c chain) Add(...any) error // for each coin, if switch type coin then add coin.id, if uint then add the id
 
-func (c chain) Remove(any) error
+// func (c chain) Remove(any) error
 
-func (c chain) Modify(any, uint32, string) error
+// func (c chain) Modify(any, uint32, string) error
 
-// No read methods, fields are exported and public
+// // No read methods, fields are exported and public
 
-func (c chain) Convert(any, any) (int, error) // accepts coin, coin.id, or Coins[] index
+// func (c chain) Convert(any, any) (int, error) // accepts coin, coin.id, or Coins[] index
 
-func (c chain) String() string
+// func (c chain) String() string
 
-// Coin methods----------------------------
+// // Coin methods----------------------------
 
-func (c coin) New(string, uint32)
+// func (c coin) New(string, uint32)
 
-func (c coin) Modify(string, uint32)
+// func (c coin) Modify(string, uint32)
 
-// coinTable methods-------------------------
-func (ct coinTable) Last() uint16
+// // coinTable methods-------------------------
+// func (ct coinTable) Last() uint16
 
-func (ct coinTable) First() uint16
+// func (ct coinTable) First() uint16
 
-func (ct coinTable) Exist(any) bool
+// func (ct coinTable) Exist(any) bool
 
-// coinChain methods-------------------------
-func (ct chainTable) Last() uint16
+// // coinChain methods-------------------------
+// func (ct chainTable) Last() uint16
 
-func (ct chainTable) First() uint16
+// func (ct chainTable) First() uint16
 
-func (ct chainTable) Exist(any) bool
-
+// func (ct chainTable) Exist(any) bool
